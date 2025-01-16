17da83860dfe78757410c342395f6d9ea5ec2db9
FliegendeWurst <2012gdwu+github@posteo.de>
2025-01-16 23:35:19 +0100

    Tweak test server setup wait

--- setup-test-server.sh
+++ setup-test-server.sh
@@ -2,6 +2,6 @@
 
 export HOME=$(mktemp -d)
 trilium-server & disown
-sleep 1
+sleep 10
 curl 'http://localhost:8080/api/setup/new-document' -X POST
 curl 'http://localhost:8080/set-password' -X POST --data-raw 'password1=1234&password2=1234'
