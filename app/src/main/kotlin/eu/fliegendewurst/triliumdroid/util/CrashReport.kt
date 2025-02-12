4a2849680ef657021065969b45daf41c4adb3b29
Arne Keller <arne.keller@posteo.de>
2025-02-12 22:15:38 +0100

    Include logs in crash report

--- app/src/main/kotlin/eu/fliegendewurst/triliumdroid/util/CrashReport.kt
+++ app/src/main/kotlin/eu/fliegendewurst/triliumdroid/util/CrashReport.kt
@@ -2,9 +2,12 @@ package eu.fliegendewurst.triliumdroid.util
 
 import android.content.Context
 import eu.fliegendewurst.triliumdroid.Cache
+import java.io.BufferedReader
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.io.InputStreamReader
+
 
 object CrashReport {
 	fun saveReport(context: Context, thread: Thread, throwable: Throwable) {
@@ -14,10 +17,32 @@ object CrashReport {
 		}
 		val file = File(crashreportDir, Cache.utcDateModified())
 		val fos = FileOutputStream(file)
+
 		try {
 			val bytes =
 				"\n\nThread: ${thread.name} (ID = ${thread.id})\nError: ${throwable}\nStacktrace:\n${throwable.stackTraceToString()}".encodeToByteArray()
 			fos.write(bytes)
+		} catch (e: IOException) {
+			// ignored
+		}
+		try {
+			val process = Runtime.getRuntime().exec("logcat -d")
+			val bufferedReader = BufferedReader(
+				InputStreamReader(process.inputStream)
+			)
+			val logcat = bufferedReader.readText()
+			var logcatData = logcat.encodeToByteArray()
+			if (logcatData.size > 110000) {
+				logcatData = logcatData.sliceArray(logcatData.size - 100000 until logcatData.size)
+			}
+			fos.write("\nLogcat:\n".encodeToByteArray())
+			fos.write(logcatData)
+			fos.close()
+			bufferedReader.close()
+		} catch (e: IOException) {
+			// ignored
+		}
+		try {
 			fos.close()
 		} catch (e: IOException) {
 			// ignored
