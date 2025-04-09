a25f4c660835a62ed3d07f77f1fa8728e732c7e7
FliegendeWurst <2012gdwu+github@posteo.de>
2025-04-09 15:07:01 +0200

    Make test script configurable

--- app/test/setup-test-server.sh
+++ app/test/setup-test-server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 export HOME=$(mktemp -d)
-export TRILIUM_PORT=8080
+export TRILIUM_PORT=${TRILIUM_PORT:-8080}
 trilium-server & disown
-sleep 10
-curl 'http://localhost:8080/api/setup/new-document' -X POST
-curl 'http://localhost:8080/set-password' -X POST --data-raw 'password1=1234&password2=1234'
+sleep ${SETUP_SLEEP:-10}
+curl "http://localhost:$TRILIUM_PORT/api/setup/new-document" -X POST
+curl "http://localhost:$TRILIUM_PORT/set-password" -X POST --data-raw "password1=1234&password2=1234"
