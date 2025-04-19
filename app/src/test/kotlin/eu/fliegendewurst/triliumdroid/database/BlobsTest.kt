a3710cbdf6377b6d12c0f298c26e5494072cd6d3
FliegendeWurst <2012gdwu+github@posteo.de>
2025-04-19 22:14:24 +0200

    Fix unit test

--- app/src/test/kotlin/eu/fliegendewurst/triliumdroid/database/BlobsTest.kt
+++ app/src/test/kotlin/eu/fliegendewurst/triliumdroid/database/BlobsTest.kt
@@ -6,12 +6,16 @@ import org.junit.Test
 class BlobsTest {
 	@Test
 	fun contentHashIsCorrect() {
-		assertEquals("z4PhNX7vuL3xVChQ1m2A", Blobs.calcHash(null))
-		assertEquals("z4PhNX7vuL3xVChQ1m2A", Blobs.calcHash(byteArrayOf()))
-		assertEquals("7SsREyHyFYSXB6DcrHza", Blobs.calcHash(("<div>\n" +
-				"          <div>\n" +
-				"            <p>This is a place I use to put notes waiting for better categorization</p>\n" +
-				"          </div>\n" +
-				"        </div>").encodeToByteArray()))
+		assertEquals("z4PhNX7vuL3xVChQ1m2A", Blobs.calcHash(null, false).id)
+		assertEquals("z4PhNX7vuL3xVChQ1m2A", Blobs.calcHash(byteArrayOf(), false).id)
+		assertEquals(
+			"7SsREyHyFYSXB6DcrHza", Blobs.calcHash(
+				("<div>\n" +
+						"          <div>\n" +
+						"            <p>This is a place I use to put notes waiting for better categorization</p>\n" +
+						"          </div>\n" +
+						"        </div>").encodeToByteArray(), false
+			).id
+		)
 	}
 }
